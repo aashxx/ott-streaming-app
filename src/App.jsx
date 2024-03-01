8a27fa525e59ea18ae4636ceaf0e19b11cec42ec
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-03-01 22:52:48 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
