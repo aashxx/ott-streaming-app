c3e89495bc41bd518a17fe62c6eddd36dcb994b6
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-03-31 19:04:24 +0530

    done

--- src/admin/components/Layout.jsx
+++ src/admin/components/Layout.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import { styled } from 'styled-components';
 
 const Layout = ({ children }) => {
   return (
-    <div style={{ backgroundColor: "white", width: "100%" }}>
+    <>
+    <Div>
         <Sidebar />
         <main>
             { children }
         </main>
-    </div>
+    </Div>
+    <Mobile>
+      Not supported in mobile devices
+    </Mobile>
+    </>
   )
 }
+
+const Div = styled.div`
+  background-color: white;
+  width: 100%;
+
+  @media screen and (max-width: 810px) {
+    display: none;
+  }
+`;
+
+const Mobile = styled.div`
+  display: none;
+
+  @media screen and (max-width: 810px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    color: white;
+    font-weight: bold;
+  }
+`;
 
 export default Layout;
